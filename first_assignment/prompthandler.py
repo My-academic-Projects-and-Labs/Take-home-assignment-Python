e23217a67012c9b1ab73b931ff5864a659f3ea85
Damika <damikaanupama@gmail.com>
2023-10-15 22:44:10 +0530

    Update first_assignment code

--- first_assignment/prompthandler.py
+++ first_assignment/prompthandler.py
@@ -5,6 +5,7 @@ import json
 from parameters import ParameterManager
 from transcripts import TranscriptManager
 
+
 class PromptHandler:
     def __init__(self):
         self.parameter_manager = ParameterManager()
@@ -20,10 +21,9 @@ class PromptHandler:
         print("Available formats: Text, HTML")
         self.format = input("Output Format: ")
 
-
     def translate_prompt(self, prompt):
         # Issue getting the permission of the system to install the requirements
-        
+
         # if self.language == "Spanish":
         #     install_requirements()
         #     target_language='ES'
@@ -42,7 +42,8 @@ class PromptHandler:
 
     def get_formatted_prompt(self):
         transcript = self.transcript_manager.get_transcript()
-        formatted_prompt = transcript.format(**self.parameter_manager.parameters)
+        formatted_prompt = transcript.format(
+            **self.parameter_manager.parameters)
         translated_prompt = self.translate_prompt(formatted_prompt)
         return translated_prompt
 
@@ -53,34 +54,24 @@ class PromptHandler:
 
         if self.format == "HTML":
             filename = "prompt.html"
-            html_template = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Coaching Prompt</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-        <h1 class="mt-4 mb-4">Coaching Prompt</h1>
-        <div class="card">
-            <div class="card-body">
-                {formatted_prompt}
-            </div>
-        </div>
-    </div>
-</body>
-</html>
-            """
-            output = html_template.format(formatted_prompt=output.replace("\n", "<br/>"))
+
+            # Read the HTML template from file
+            with open('prompt_template.html', 'r') as f:
+                html_template = f.read()
+
+            output = html_template.format(
+                formatted_prompt=output.replace("\n", "<br/>"))
         else:
             filename = "prompt.txt"
 
         with open(os.path.join(path, filename), 'w') as f:
             f.write(output)
 
+
 def install_requirements():
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+    subprocess.check_call([sys.executable, '-m', 'pip',
+                          'install', '-r', 'requirements.txt'])
+
 
 if __name__ == "__main__":
     try:
